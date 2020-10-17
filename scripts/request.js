dc33cc14a6e33965493af6eb0ee5c72e2833f37c
keerthurajeev9 <keerthana.rajeev100@gmail.com>
2020-10-17 22:29:21 +0530

    added js

---
+++ scripts/request.js
@@ -0,0 +1,15 @@
+//Using async and await
+
+const getPuzzle = async(count) => {
+    const response = await fetch(`//puzzle.mead.io/puzzle?wordCount=${count}`)
+    if(response.status===200){
+        let asyncdata = await response.json()
+        return asyncdata.puzzle
+    }
+    else{
+        throw new Error('Unable to find the link')
+    }
+}
+
+
+
